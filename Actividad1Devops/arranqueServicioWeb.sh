ba907cc46ed3e38ff20751b644ff57aba25cf8b8
crisgarlag <78167097+crisgarlag@users.noreply.github.com>
2022-02-12 12:43:22 +0100

    Delete Actividad1Devops directory

--- Actividad1Devops/arranqueServicioWeb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-arrancarServicioWeb(){
-   mvn spring-boot:run
-}
-arrancarServicioWeb
--- Actividad1Devops/modificarPuerto.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-modificarPuerto(){
-   vim /Users/cristiangarcialagar/segundo_curso/webServiceDevops/src/main/resources/application.properties
-}
-modificarPuerto
-
